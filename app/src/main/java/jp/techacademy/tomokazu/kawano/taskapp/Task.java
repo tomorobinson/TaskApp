fcbccc384f1901783c063fae38a28ca83895430e
tomorobinson <kwp_tk@hotmail.com>
2018-09-30 21:04:59 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/tomokazu/kawano/taskapp/Task.java
@@ -0,0 +1,58 @@
+package jp.techacademy.tomokazu.kawano.taskapp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Task extends RealmObject implements Serializable {
+    private String title; // タイトル
+    private String contents; // 内容
+    private String category; // カテゴリー
+    private Date date; // 日時
+
+    // idをプライマリキーとして設定
+    @PrimaryKey
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
